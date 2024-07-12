ba8202ce228fb79eb3bc8c388021617f185456ef
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-12 22:58:39 +0200

    added slider for rating

--- TVShows/app.js
+++ TVShows/app.js
@@ -1,4 +1,8 @@
 const form = document.querySelector('form');
+const sortBtn = document.getElementById('sortBtn');
+const ratingSlider = document.getElementById('rating');
+const ratingValue = document.getElementById('ratingValue');
+let searchData = [];
 
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
@@ -12,37 +16,55 @@ form.addEventListener('submit', async function(e) {
         }
 
         const data = await response.json();
+        searchData = data; 
         tvShowsSearch(data);
     } catch (error) {
         console.error('Fetch error:', error);
     }
 });
 
+sortBtn.addEventListener('click', function() {
+    const sortedData = sortShowsByRating();
+    tvShowsSearch(sortedData);
+});
+
+ratingSlider.addEventListener('input', function() {
+    ratingValue.textContent = ratingSlider.value;
+});
+
 const tvShowsSearch = (data) => {
     const divShow = document.querySelector('.showing');
 
-    // Prvo uklanjamo sve prethodno prikazane slike
+
     while (divShow.firstChild) {
         divShow.removeChild(divShow.firstChild);
     }
 
-    // Dodajemo nove slike
     data.forEach(d => {
-        if (d.show.image) {
+        if (d.show.image && (!ratingSlider.value || d.show.rating.average >= ratingSlider.value)) {
             const container = document.createElement('div');
-            container.classList.add('show-container'); // Dodajemo klasu za stilizaciju
-
+            container.classList.add('show-container'); 
             const img = document.createElement('img');
             img.src = d.show.image.medium;
-            img.classList.add('show-image'); // Dodajemo klasu za stilizaciju
+            img.classList.add('show-image'); 
 
             const span = document.createElement('span');
             span.innerText = d.show.name;
-            span.classList.add('show-name'); // Dodajemo klasu za stilizaciju
-
+            span.classList.add('show-name'); 
             container.appendChild(img);
             container.appendChild(span);
             divShow.appendChild(container);
         }
     });
+};
+
+const sortShowsByRating = () => {
+    const searchTerm = form.elements.query.value;
+    const filteredData = searchTerm ? searchData.filter(d => d.show.name.toLowerCase().includes(searchTerm.toLowerCase())) : searchData;
+    const sortedData = filteredData.sort((a, b) => {
+        if (!a.show.rating.average) return 1;
+        if (!b.show.rating.average) return -1;
+        return b.show.rating.average - a.show.rating.average;
+    });
+    return sortedData;
 };
